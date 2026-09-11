0c5417bf0ca662dab76fa9a1cd01948b41c9b6f1
sudheebhaskar <sudheebhaskar@gmail.com>
2026-09-11 11:47:17 +0530

    minor chages

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,21 +1,4 @@
-//  import useFetch from "../useFetch"
- 
-//  const Books = () => {
 
-//     const { data, loading, error } = useFetch("https://be4assignment1-one.vercel.app/books");
-
-
-//     return(
-//         <div>
-//             <h1>All Books</h1>
-//             <ul>
-//             { data?.map(book => <li> {book.title}</li>)}
-//             </ul>
-//         </div>
-//     )
-//  }
-
-//  export default Books;
 
 import { useState } from "react";
 import useFetch from "../useFetch";
